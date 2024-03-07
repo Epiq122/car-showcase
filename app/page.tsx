b7e7d0e5bb26f8856858ab2b71ca16424e17a2c8
Robert Gleason <l2ob122@gmail.com>
2024-03-06 19:02:27 -0800

    Add CarCard component and update Home page layout,

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { CustomFilter, Hero, SearchBar } from '@/components';
+import { CarCard, CustomFilter, Hero, SearchBar } from '@/components';
 import { fetchCars } from '@/utils';
 
 export default async function Home() {
@@ -21,7 +21,13 @@ export default async function Home() {
           </div>
         </div>
         {!isDataEmpty ? (
-          <section>WE HAVE CARS</section>
+          <section>
+            <div className='home__cars-wrapper'>
+              {cars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
         ) : (
           <div className='home__error-container'>
             <h2 className='text-black text-xl font-bold'>Oops no results</h2>
